93b88a89e7cf21019d83108a99642b0223133bcb
Mark O'Toole <mark.otoole@gmail.com>
2025-08-30 16:03:59 +0100

    Add artistType to schema exports

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,7 +5,8 @@ import {categoryType} from './categoryType'
 import {postType} from './postType'
 import {authorType} from './authorType'
 import {pageType} from './pageType'
+import {artistType} from './artistType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, pageType],
+  types: [blockContentType, categoryType, postType, authorType, pageType, artistType],
 }
