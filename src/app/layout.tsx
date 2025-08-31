b453e808e2b9a4d5fa297b838995093e8973bb5a
Mark O'Toole <mark.otoole@gmail.com>
2025-08-31 10:46:32 +0100

    Update footer copyright year from 2024 to 2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,7 +105,7 @@ function Footer() {
   return (
     <footer className="bg-black text-white p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>© 2024 Boogie. All rights reserved.</p>
+        <p>© 2025 Boogie. All rights reserved.</p>
       </div>
     </footer>
   );
