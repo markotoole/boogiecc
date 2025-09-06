fdeca1dc1a72c571d38bd71edd67998586624316
Mark O'Toole <mark.otoole@gmail.com>
2025-09-06 10:13:04 +0100

    Add customBlockType to schema imports and ensure correct order

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -6,7 +6,8 @@ import {postType} from './postType'
 import {authorType} from './authorType'
 import {pageType} from './pageType'
 import {artistType} from './artistType'
+import customBlockType from './customBlockType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, pageType, artistType],
+  types: [customBlockType, blockContentType, categoryType, postType, authorType, pageType, artistType],
 }
